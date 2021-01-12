a3185e36303e8443eba89d82478f1fe5e9016abb
Andre Silva <andrepgsilva@outlook.com>
2021-01-12 15:41:12 +0000

    feat: Add match password token and forgot password

--- src/pages/ResetPassword.tsx
+++ src/pages/ResetPassword.tsx
@@ -10,18 +10,18 @@ import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch } from '../store';
 import * as SecureStore from 'expo-secure-store';
 import {
-  getCodeToRecoverPassword,
   clearServerAuthErrors,
   serverLoginErrorsSelector,
-  confirmCodeToRecoverPassword
+  resetPassword
 } from '../store/reducers/AuthReducer';
 
-export default function MatchPasswordToken () {
+export default function ResetPassword () {
   const dispatch = useDispatch<AppDispatch>();
   const navigation = useNavigation();
 
   let userEmail: any = '';
-  const [passwordConfirmationToken, setPasswordConfirmationToken] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirmation, setPasswordConfirmation] = useState('');
   const serverLoginErrors = useSelector(serverLoginErrorsSelector);
 
   SecureStore.getItemAsync('userEmail')
@@ -32,34 +32,34 @@ export default function MatchPasswordToken () {
   const getConfirmationCode = () => {
     dispatch(clearServerAuthErrors());
 
-    dispatch(confirmCodeToRecoverPassword({ password_confirmation_token: passwordConfirmationToken, email: userEmail }))
+    dispatch(resetPassword({ password, email: userEmail }))
       .then((asyncThunkResponse: object) => {
         const hasNotAserverError = !hasOwnProperty(asyncThunkResponse, 'error');
         const hasFormFieldsErrors = !hasOwnProperty(asyncThunkResponse, 'errors');
 
         if (hasNotAserverError && hasFormFieldsErrors) {
-          navigation.navigate('ResetPassword');
+          navigation.navigate('Login');
         }
       });
   };
 
-  const isButtonDisabled = passwordConfirmationToken === '';
+  const isPasswordsDifferent = password !== passwordConfirmation;
+  const isButtonDisabled = password === '' || passwordConfirmation === '' || isPasswordsDifferent;
   const disabledClass = isButtonDisabled ? _styles['bg-main-color-light'] : _styles['bg-main-color'];
 
   return (
     <View
       style={[_styles.flexOne]}
     >
-      <View style={[_styles.itemsCenter, _styles['mt-100']]}>
+      <View style={[_styles.itemsCenter, _styles['mt-90']]}>
         <View
           style={[
-            { width: '85%' },
-            _styles['mt-10']
+            { width: '85%' }
           ]}
         >
           <View style={[_styles.itemsCenter]}>
             <Ionicons
-              name="mail-unread-outline"
+              name="lock-closed-outline"
               color={_styles['text-main-color'].color}
               size={140}
             />
@@ -69,20 +69,20 @@ export default function MatchPasswordToken () {
             <Text
               style={[_styles.defaultFontStyleBold, _styles['text-cool-gray-700']]}
             >
-              { i18n.t('auth.enter_code_confirmation_code') }
+              { i18n.t('auth.you_can_recover_your_password') }
             </Text>
           </View>
 
           <View>
             <Ionicons
-              name="key-outline"
+              name="md-lock-open-outline"
               color={_styles['text-cool-gray-400'].color}
               size={20}
               style={[_styles.positionAbsolute, { top: 45 }, { left: 20 }, _styles.zIndexTwo]}
             />
             <TextInput
-              value={passwordConfirmationToken}
-              onChangeText={text => setPasswordConfirmationToken(text)}
+              value={password}
+              onChangeText={text => setPassword(text)}
               style={[
                 _styles.zIndexOne,
                 _styles['mt-25'],
@@ -95,8 +95,37 @@ export default function MatchPasswordToken () {
                 _styles.brThirty,
                 _styles.defaultFontStyleSemiBold
               ]}
-              placeholder={ i18n.t('auth.confirmation_code') }
+              secureTextEntry={true}
+              placeholder={ i18n.t('auth.password') }
               placeholderTextColor={_styles['text-cool-gray-100'].color}
+            ></TextInput>
+          </View>
+
+          <View>
+            <Ionicons
+              name="md-lock-open-outline"
+              color={_styles['text-cool-gray-400'].color}
+              size={20}
+              style={[_styles.positionAbsolute, { top: 45 }, { left: 20 }, _styles.zIndexTwo]}
+            />
+            <TextInput
+              value={passwordConfirmation}
+              onChangeText={text => setPasswordConfirmation(text)}
+              style={[
+                _styles.zIndexOne,
+                _styles['mt-25'],
+                _styles['pl-50'],
+                _styles['pt-16'],
+                _styles['pb-16'],
+                _styles['bg-white'],
+                _styles['text-cool-gray-800'],
+                _styles['w-full'],
+                _styles.brThirty,
+                _styles.defaultFontStyleSemiBold
+              ]}
+              placeholder={ i18n.t('auth.password_confirmation') }
+              placeholderTextColor={_styles['text-cool-gray-100'].color}
+              secureTextEntry={true}
             ></TextInput>
           </View>
 
@@ -117,7 +146,7 @@ export default function MatchPasswordToken () {
               _styles.textCenter,
               _styles.textFifteen
             ]}>
-              { i18n.t('auth.confirm_the_code') }
+              { i18n.t('auth.reset_password') }
             </Text>
           </TouchableOpacity>
         </View>
